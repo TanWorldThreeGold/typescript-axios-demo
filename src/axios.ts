ac43fa90710dfd9c47c870f414e08a1ca5f0be01
han <18503030864@163.com>
2020-03-19 22:25:54 +0800

    axios基本功能

---
+++ src/axios.ts
@@ -0,0 +1,28 @@
+import { AxiosRequestConfig, AxiosStatic } from './types/index'
+import Axios from './core/Axios'
+import { extend } from './helpers/util'
+import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
+import CancelToken from './cancel/CancelToken'
+import Cancel, { isCancel } from './cancel/Cancel'
+
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
+  const context = new Axios(config)
+  let instance = Axios.prototype.request.bind(context)
+
+  instance = extend(instance, context)
+
+  return instance as AxiosStatic
+}
+
+const axios = createInstance(defaults)
+
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
+
+axios.CancelToken = CancelToken
+axios.Cancel = Cancel
+axios.isCancel = isCancel
+
+export default axios
